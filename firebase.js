b697714c2797a44b93476e4c4a9ea28d04e54030
calcmp <calcmp@gmail.com>
2019-05-19 20:11:00 +0100

    first commit

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 import "@firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyB7L6joG4nyg6XyOdKHKeJORVeaNtS326g",
-  authDomain: "band-app-8da62.firebaseapp.com",
-  databaseURL: "https://band-app-8da62.firebaseio.com",
-  projectId: "band-app-8da62",
-  storageBucket: "band-app-8da62.appspot.com",
-  messagingSenderId: "1084918838624"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
